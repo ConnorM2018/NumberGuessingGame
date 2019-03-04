ce4e7aba26d7e8ff96d3be62469ebaaec568ee71
Unknown <connor.mcnutt.2022@student.lhschools.org>
2019-03-04 14:03:24 -0500

    Initialized variables and added introduction.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,19 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        int guess;
+        int pickedNumber;
+        int attempts;
+        pickedNumber = (int)(Math.random()*100);
+        attempts = 1;
+        System.out.println(pickedNumber);
+        System.out.println("Welcome to my Number Guessing Game! To start, please guess a number between 1 and 100.");
+        guess = keyboard.nextInt();
     }
 }
